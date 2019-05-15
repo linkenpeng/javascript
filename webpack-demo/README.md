5efbb044b82df2003bc21f4f4953bfcd32d13c25
peter <collin_linken@qq.com>
2019-05-15 18:52:55 +0800

    管理输出

--- webpack-demo/README.md
+++ webpack-demo/README.md
@@ -8,4 +8,7 @@ npm install --save-dev file-loader
 
 npm install --save-dev csv-loader xml-loader
 
+npm install --save-dev html-webpack-plugin
+
+
 ```
--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,19 +1,32 @@
 import _ from 'lodash';
+
+/*
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
+*/
+
+import printMe from './print.js';
 
 function component() {
   let element = document.createElement('div');
 
+  var btn = document.createElement('button');
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = '点击这里，然后查看console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
+  /* 
   element.classList.add('hello');
-  
   var myIcon = new Image();
   myIcon.src = Icon;
   element.appendChild(myIcon);
 
   console.log(Data);
+  */
 
   return element;
 }
