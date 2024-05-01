443a9d2123314624fcbbe804195d3d66d5f2d656
linkenpeng <collin_linken@qq.com>
2024-05-01 14:59:03 +0800

    remove intadmin

--- vue/project/intadmin/src/components/OffiWeb.vue
+++
@@ -1,9 +0,0 @@
-
-<template>
-    <div>
-      <el-link href="https://cn.vuejs.org/" target="_blank">Vue</el-link>
-    </div>
-    <div>
-      <el-link href="https://element-plus.org/zh-CN/component/overview.html" target="_blank">Element-Component</el-link>
-    </div>
-</template>
